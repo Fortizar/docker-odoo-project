3c44927f238bdfc5298ff1aa51d3569f4c1c6931
Szabolcs Máj <szabolcs.maj@gmail.com>
2021-08-28 14:29:14 +0200

    Simplified builds and only publish when on master

--- travis/publish.sh
+++ travis/publish.sh
@@ -6,14 +6,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
-    if [ "$VERSION" == "14.0" ]; then
-      make VERSION=$VERSION TAG=$TRAVIS_TAG tag_latest_main push_latest_main
-    fi
-    make VERSION=$VERSION TAG=latest tag push
-  elif [ ! -z "$TRAVIS_TAG" ]; then
     make VERSION=$VERSION TAG=$TRAVIS_TAG tag push
+    make VERSION=$VERSION TAG=latest tag push
   else
-    echo "Not pushing any image"
+    echo "Not on master -> not pushing any image"
   fi
-
 fi
