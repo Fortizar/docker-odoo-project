9707a181e65c71077603d2292be4d3cfda2a70b8
Szabolcs Máj <szabolcs.maj@gmail.com>
2021-09-15 13:56:01 +0200

    Travis gets the travis version from file, instead of travis envvar

--- travis/publish.sh
+++ travis/publish.sh
@@ -5,6 +5,7 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
+  TRAVIS_TAG=$(cat ./TRAVIS_TAG)
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     make VERSION=$VERSION TAG=$TRAVIS_TAG tag push
     make VERSION=$VERSION TAG=latest tag push
