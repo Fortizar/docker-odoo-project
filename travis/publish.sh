848e1f02fdccda5897b1fa396d3e65fee03c7899
Szabolcs Máj <szabolcs.maj@gmail.com>
2021-09-16 00:12:36 +0200

    Reading TRAVIS_TAG updated

--- travis/publish.sh
+++ travis/publish.sh
@@ -5,7 +5,8 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
-  TRAVIS_TAG=$(cat ./TRAVIS_TAG)
+  ls -la
+  TRAVIS_TAG=$(cat ./travis/TRAVIS_TAG)
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     make VERSION=$VERSION TAG=$TRAVIS_TAG tag push
     make VERSION=$VERSION TAG=latest tag push
