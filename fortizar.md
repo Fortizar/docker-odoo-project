9515553b63d072ee99fa43a3936f7e87ce7d8823
Szabolcs Máj <szabolcs.maj@gmail.com>
2024-10-02 17:46:02 +0200

    Bumped up version to 4.3.0 with new debian version. This required bumping a lot of dependency versions and removing a few

---
+++ fortizar.md
@@ -0,0 +1,4 @@
+# Build
+
+Run it like this: `VERSION=13.0 IMAGE_LATEST=fortizarhu/odoo-project:13.0-4.3.0 ./build.sh`
+
--- install/postgres.sh
+++ install/postgres.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-curl -s https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+#curl -s https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb [signed-by=/usr/share/keyrings/pgdg-archive-keyring.gpg] http://apt.postgresql.org/pub/repos/apt bullseye-pgdg main" > /etc/apt/sources.list.d/pgdg.list 
+curl -s https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /usr/share/keyrings/pgdg-archive-keyring.gpg
+
 apt-get update
 apt-get install -y --no-install-recommends postgresql-client libpq-dev
 apt-get -y install -f --no-install-recommends
+apt-get clean
